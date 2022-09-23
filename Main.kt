61c6ed74f1b9b0fb45c473bf00eb5d93d4308238
kimnamhyeon <knh306@naver.com>
2022-09-24 04:09:55 +0900

    Feature_Week03_Linked_List:insert_ver2

--- Main.kt
+++ Main.kt
@@ -2,8 +2,9 @@ fun main()
 {
     val list = LinkedList<Int>()
     list.append(10).append(30).append(20)
-    println(list)
-
-    println(list.nodeAt(2))
-    println(list.nodeAt(3))
+    println("Before inserting: $list")
+    var middleNode = list.nodeAt(1)!!
+    for (i in 1..3)
+        middleNode = list.insert(-1 * i, middleNode)
+    println("After inserting: $list")
 }
