834d86ab407763a29196a2ec5ed707553152d8df
kimnamhyeon <knh306@naver.com>
2022-09-24 01:22:54 +0900

    Feature_Week03_Data_Class멤버함수override3

--- Main.kt
+++ Main.kt
@@ -1,4 +1,5 @@
-class Main {
+class Main
+{
 }
 
 fun main()
--- TestDataClass.kt
+++ TestDataClass.kt
@@ -4,4 +4,15 @@ data class TestDataClass(
 )
 {
     var c: Boolean = false
+
+    override fun equals(other: Any?): Boolean
+    {
+        if (other == null || other !is TestDataClass)
+            return false
+        return this.a == other.a && this.b == other.b && this.c == other.c
+    }
+    override fun toString(): String
+    {
+        return "TestDataClass(a=$a, b=$b, c=$c)"
+    }
 }
