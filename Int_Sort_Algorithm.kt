f2796e95b95677c73d131533dfc7b80d7a362ad4
kimnamhyeon <knh306@naver.com>
2022-09-24 03:34:44 +0900

    Feature_Week03_Space_Complexity

---
+++ Int_Sort_Algorithm.kt
@@ -0,0 +1,12 @@
+fun printSorted(numbers: List<Int>)
+{
+    val sorted = numbers.sorted()
+    for (element in sorted)
+        println(element)
+}
+
+fun main()
+{
+    val numbers = listOf(1,-5,3,56,66,68,80,99,105,450)
+    printSorted(numbers)
+}
