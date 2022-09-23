4db289feb2d0584d40d15640580936eedf76c5d2
kimnamhyeon <knh306@naver.com>
2022-09-24 03:57:39 +0900

    Feature_Week03_Linked_List:push_ver1

---
+++ LinkedList.kt
@@ -0,0 +1,24 @@
+class LinkedList<T>
+{
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+    fun isEmpty(): Boolean{
+            return size == 0
+    }
+
+    override fun toString(): String {
+        if (isEmpty()) {
+            return "Empty List"
+        }
+        return head.toString()
+    }
+
+    fun push(value: T)
+    {
+        head = Node(value = value, next = head)
+        if (tail == null)
+            tail = head
+        size++
+    }
+}
