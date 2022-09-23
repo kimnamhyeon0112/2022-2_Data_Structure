1ad5e304cf81485ec25cd1fd9b80a0f29770a3f4
kimnamhyeon <knh306@naver.com>
2022-09-24 01:10:45 +0900

    Feature_Week03_Data_Class자동 생성된 함수 사용해보기

--- Main.kt
+++ Main.kt
@@ -5,5 +5,14 @@ class Main {
 fun main()
 {
     var o1: TestDataClass = TestDataClass(3)
+    var o2: TestDataClass = TestDataClass(5, "hello")
+    val o3: TestDataClass = o2.copy()
+
+    val eq: Boolean = o1.equals(o2)
+    println(eq)
+    println(o2.equals(o3))
+
+    var (a, b) = o1
+    println("$a\t$b")
     println(o1)
 }
--- TestDataClass.kt
+++ TestDataClass.kt
@@ -2,4 +2,6 @@ data class TestDataClass(
     var a: Int,             // getter, setter 자동생성
     val b: String="Empty"   // getter 자동생성
 )
-{}
+{
+
+}
