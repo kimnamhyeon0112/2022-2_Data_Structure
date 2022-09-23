e056b216d6e7942d44320ffd6a13cae8b500e52e
kimnamhyeon <knh306@naver.com>
2022-09-24 01:36:22 +0900

    Feature_Week03_Interface

--- Main.kt
+++ Main.kt
@@ -4,8 +4,10 @@ class Main
 
 fun main()
 {
-    var o1: TestAbstClass = ChildOfAbstClass()
+    var o1: ChildOfInterface = ChildOfInterface(10)
     println(o1)
     println(o1.f1())
-    println(o1.f2())
+    println(o1.f2("A"))
+    println(o1.f3("B"))
+    println(o1.f4())
 }
