1a5f281381cbc009f2808f2fe7a02d4c6f9a6fda
kimnamhyeon <knh306@naver.com>
2022-09-24 13:49:48 +0900

    Feature_Week04_Linked_List:개량하기ver1

--- Main.kt
+++ Main.kt
@@ -1,11 +1,19 @@
 fun main()
 {
     val list = LinkedList<Int>()
-    list.append(10).append(30).append(20)
-    println("Before removing at particular index: $list")
-    val index = 1
-    val node = list.nodeAt(index - 1)!!
-    val removedValue = list.removeAfter(node)
-    println("After removing at index: $index: $list")
-    println("Removed value: $removedValue")
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    println(list)
+    var the_node = list.nodeAt(0)!!
+    while(true)
+    {
+        print("${the_node.value * 2}")
+        if(the_node.next != null)
+        {
+            print(" -> ")
+            the_node = the_node.next!!
+        }
+        else break
+    }
 }
