0eae53d955f880f3a7c40878bf7f231a5c7f2e36
kimnamhyeon <knh306@naver.com>
2022-09-25 19:25:09 +0900

    Feature_Week04_Inner_vs_Nested_vs_Companion

--- Main.kt
+++ Main.kt
@@ -1,7 +1,13 @@
 fun main() {
-    var s1 = Smartphone("A32")
-    s1.Inner().which_cpu()
-    Smartphone.Companion.which_cpu()
-    val outer = Outer()
-    Outer.Nested().accessCompanionMethod()
+    // 전자가 존재해야 후자가 존재함
+    var i1 = Smartphone("A32")
+    var i2 = i1.Inner()
+
+    // 남남인 관계
+    var o1 = Outer()
+    var o2 = Outer.Nested()
+
+    // 후자는 전자가 존재하지 않더라도 상관없음
+    var c1 = Smartphone("S7")
+    var c2 = Smartphone.Companion
 }
