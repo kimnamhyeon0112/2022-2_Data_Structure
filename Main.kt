4e653729de2a8894e468d0071ef680aa42950b10
kimnamhyeon <knh306@naver.com>
2022-09-25 19:01:12 +0900

    Feature_Week04_Stack:응용하기

--- Main.kt
+++ Main.kt
@@ -1,13 +1,20 @@
+fun String.checkParentheses(): Boolean {
+    val stack = Stack<Char>()
+    for (character in this) {
+        when (character) {
+            '(' -> stack.push(character)
+            ')' -> if (stack.isEmpty)
+                return false
+            else
+                stack.pop()
+        }
+    }
+    return stack.isEmpty
+}
+
 fun main()
 {
-    val stack = Stack<Int>().apply{
-        push(1)
-        push(2)
-        push(3)
-        push(4)
-    }
-
-    print(stack)
-    println(stack.peek())
-    print(stack)
+    val s = "h((e))llo(world)()"
+    println(s.checkParentheses())
+    println("(hello world".checkParentheses())
 }
