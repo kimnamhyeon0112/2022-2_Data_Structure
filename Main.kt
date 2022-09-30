805dec02e997e1b5d34e6821bf07d9a5eabc4fda
kimnamhyeon <knh306@naver.com>
2022-09-30 21:43:50 +0900

    Feature_Week05_Tree_Node

--- Main.kt
+++ Main.kt
@@ -1,12 +1,8 @@
-import java.util.Calendar
-
-fun main() {
-    val calendar = Calendar.getInstance()
-    if(calendar.firstDayOfWeek==Calendar.SUNDAY){
-        calendar.firstDayOfWeek=Calendar.MONDAY
+fun main(){
+    val hot = TreeNode("Hot")
+    val cold = TreeNode("Cold")
+    val beverage = TreeNode("Beverages").run{
+        add(hot)
+        add(cold)
     }
-    if(!calendar.isLenient) {
-        calendar.isLenient = true
-    }
-    println(calendar)
 }
---
+++ TreeNode.kt
@@ -0,0 +1,5 @@
+class TreeNode<T> (val value: T) {
+    private val children: MutableList<TreeNode<T>> = mutableListOf()
+
+    fun add(child: TreeNode<T>) = children.add(child)
+}
