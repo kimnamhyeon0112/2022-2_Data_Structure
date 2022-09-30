4d375a9392f2bd34d3c401dde35d392e01bc0652
kimnamhyeon <knh306@naver.com>
2022-09-30 21:48:18 +0900

    Feature_Week05_Trees:Traversal_algorithms-DFT

--- Main.kt
+++ Main.kt
@@ -1,8 +1,34 @@
-fun main(){
-    val hot = TreeNode("Hot")
-    val cold = TreeNode("Cold")
-    val beverage = TreeNode("Beverages").run{
-        add(hot)
-        add(cold)
-    }
+fun makeBeverageTree(): TreeNode<String> {
+    val tree = TreeNode("Beverage")
+    val hot = TreeNode("hot")
+    val cold = TreeNode("cold")
+    val tea = TreeNode("tea")
+    val coffee = TreeNode("coffee")
+    val chocolate = TreeNode("cocoa")
+    val blackTea = TreeNode("black")
+    val greenTea = TreeNode("green")
+    val chaiTea = TreeNode("chai")
+    val soda = TreeNode("soda")
+    val milk = TreeNode("milk")
+    val gingerAle = TreeNode("ginger ale")
+    val bitterLemon = TreeNode("bitter lemon")
+
+    tree.add(hot)
+    tree.add(cold)
+    tree.add(tea)
+    tree.add(coffee)
+    tree.add(chocolate)
+    tree.add(soda)
+    tree.add(milk)
+    tree.add(blackTea)
+    tree.add(greenTea)
+    tree.add(chaiTea)
+    tree.add(gingerAle)
+    tree.add(bitterLemon)
+    return tree
+}
+
+fun main() {
+    val tree = makeBeverageTree()
+    tree.forEachDepthFirst { println(it.value) }
 }
--- TreeNode.kt
+++ TreeNode.kt
@@ -2,4 +2,13 @@ class TreeNode<T> (val value: T) {
     private val children: MutableList<TreeNode<T>> = mutableListOf()
 
     fun add(child: TreeNode<T>) = children.add(child)
+
+    fun forEachDepthFirst(visit: Visitor<T>) {
+        visit(this)     //println(visit.value)와 동일, 14번 줄의 typealias때문에 visit(this)로 출력가능
+        children.forEach {
+            it.forEachDepthFirst(visit)
+        }
+    }
 }
+
+typealias Visitor<T> = (TreeNode<T>) -> Unit
