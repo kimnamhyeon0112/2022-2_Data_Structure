6d18b218f783684acca0d3e5c566f12280136250
kimnamhyeon <knh306@naver.com>
2022-09-30 21:30:48 +0900

    Feature_Week05_forEach

--- Main.kt
+++ Main.kt
@@ -1,11 +1,17 @@
-typealias Predicate<T> = (T) -> Boolean
-
-fun foo(p: Predicate<Int>) = p(42)
-
 fun main() {
-    val f: (Int) -> Boolean = { it > 0 }
-    println(foo(f))
-
-    val p: Predicate<Int> = { it > 0 }
-    println(listOf(1, -2).filter(p))
+    var k1 = (1..6).toList()
+    k1.forEach{
+        if (it==2){return@forEach}
+        println(it*10)
+    }
+    run{
+        println("들어간다")
+        k1.subList(0, 4).forEach{
+            println(it)
+            if(it%2==0)
+                return@run
+        }
+        println("나왔다!")
+    }
+    println("끝났다!")
 }
