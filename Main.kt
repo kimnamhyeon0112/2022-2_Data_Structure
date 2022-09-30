6a48e6f71d6521795a4652ceae61d908d55fe6d6
kimnamhyeon <knh306@naver.com>
2022-09-30 21:32:19 +0900

    Feature_Week05_Elvis_operator

--- Main.kt
+++ Main.kt
@@ -1,17 +1,9 @@
+fun test(str: String?){
+    val the_str = str ?: "unknown"
+    println(the_str)
+}
+
 fun main() {
-    var k1 = (1..6).toList()
-    k1.forEach{
-        if (it==2){return@forEach}
-        println(it*10)
-    }
-    run{
-        println("들어간다")
-        k1.subList(0, 4).forEach{
-            println(it)
-            if(it%2==0)
-                return@run
-        }
-        println("나왔다!")
-    }
-    println("끝났다!")
+    test("Hello")
+    test(null)
 }
