b5738e81b43cace90d37ed34a31eabfac910098e
kimnamhyeon <knh306@naver.com>
2022-10-08 03:34:04 +0900

    Feature_Week06_AVL_Trees

--- Main.kt
+++ Main.kt
@@ -1,14 +1,23 @@
-import org.w3c.dom.css.Rect
-
-open class Shape
-class Rectangle:Shape()
-fun Shape.getName() = "shape"
-fun Rectangle.getName() = "rectangle"
-
-fun printClassName(s:Shape) {
-    println(s.getName())
+fun makeAVLTree(): AVLNode<Int> {
+    val one = AVLNode(1)
+    val seven = AVLNode(7)
+    val eight = AVLNode(8)
+    val nine = AVLNode(9)
+    val eleven = AVLNode(11)
+    val ten = AVLNode(10)
+    val fifteen = AVLNode(15)
+    seven.leftChild = one
+    seven.rightChild = nine
+    nine.leftChild = eight
+    nine.rightChild = eleven
+    eleven.leftChild = ten
+    eleven.rightChild = fifteen
+    return seven
 }
 
-fun main() {
-    printClassName(Rectangle())
+fun main()
+{
+    var tree = makeAVLTree()
+    println(tree)
+    println("height: ${tree.height()}")
 }
