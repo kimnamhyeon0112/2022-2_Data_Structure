6febd7dee4bcdf624d3747726850f047680304c8
kimnamhyeon <knh306@naver.com>
2022-10-08 03:23:38 +0900

    Feature_Week06_Extenstion_function/property

--- Main.kt
+++ Main.kt
@@ -1,27 +1,13 @@
+fun List<Int>.getHigherThan(num:Int):List<Int> {
+    val result = arrayListOf<Int>()
+    for (item in this)
+        if (item > num)
+            result.add(item)
+    return result
+}
+
 fun main() {
-    var t_hashMap = HashMap<String, Any>()
-
-    t_hashMap.put("name", "투케이")
-    t_hashMap.put("age", 28)
-    println("put 데이터: " + t_hashMap.toString())
-
-    println("name 포함 여부:" + t_hashMap.containsKey("name"))
-
-    println("name 데이터:" + t_hashMap.get("name"))
-
-    for ((key, value) in t_hashMap) {
-        println("전체: ${key}:${value}")
-    }
-
-    t_hashMap.remove("age")
-    println("remove 데이터:" + t_hashMap.toString())
-
-    t_hashMap.replace("name","케이투")
-    println("replace 데이터:" + t_hashMap.toString())
-
-    t_hashMap.clear()
-    println("clear 데이터:" + t_hashMap.toString())
-
-
-
+    val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+    val filtered = numbers.getHigherThan(3).toString()
+    System.out.println(filtered)
 }
