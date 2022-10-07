ee99535abdb5592513281d1e1c026ea19f8cceff
kimnamhyeon <knh306@naver.com>
2022-10-08 03:37:03 +0900

    Feature_Week06_AVL_Trees:Left&Right_Rotations

---
+++ AVLTree.kt
@@ -0,0 +1,22 @@
+class AVLTree<T: Comparable<T>> {
+    var root: AVLNode<T>? = null
+    override fun toString() = root?.toString() ?: "empty tree"
+
+    private fun leftRotate(node: AVLNode<T>): AVLNode<T> {
+        val pivot = node.rightChild!!
+        node.rightChild = pivot.leftChild
+        pivot.leftChild = node
+        node.height = max(node.leftHeight, node.rightHeight) + 1
+        pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+        return pivot
+    }
+
+    private fun rightRotate(node: AVLNode<T>): AVLNode<T> {
+        val pivot = node.leftChild!!
+        node.leftChild = pivot.rightChild
+        pivot.rightChild = node
+        node.height = max(node.leftHeight, node.rightHeight) + 1
+        pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+        return pivot
+    }
+}
