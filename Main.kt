9f916afa0e5f2a9d1c3ff2238dea46c53907c094
kimnamhyeon <knh306@naver.com>
2022-10-12 07:25:32 +0900

    Feature_Week07_BinarySearch

--- Main.kt
+++ Main.kt
@@ -1,12 +1,7 @@
 fun main() {
-    val trie = Trie<Char>().apply {
-        insert("car")
-        insert("card")
-        insert("care")
-        insert("cared")
-        insert("car")
-    }
-    println(trie.lists)
-    println(trie.count)
-    println(trie.isEmpty)
+    val array = arrayListOf(1, 5, 15, 17, 19, 22, 24, 31, 105, 150)
+    val search31 = array.indexOf(31)
+    val binarySearch31 = array.binarySearch(31)
+    println("indexOf(): $search31")
+    println("binarySearch(): $binarySearch31")
 }
