900cfefbc6fb2af066e046f454c53ea363c987c6
kimnamhyeon <knh306@naver.com>
2022-10-12 07:43:32 +0900

    Feature_Week07_BinarySearch-findIndices

--- Main.kt
+++ Main.kt
@@ -1,7 +1,5 @@
 fun main() {
-    val array = arrayListOf(1, 5, 15, 17, 19, 22, 24, 31, 105, 150)
-    val search31 = array.indexOf(31)
-    val binarySearch31 = array.binarySearch(31)
-    println("indexOf(): $search31")
-    println("binarySearch(): $binarySearch31")
+    val array = arrayListOf(1, 2, 3, 3, 3, 4, 5, 5)
+    val indices = array.findIndices(3)
+    println(indices)
 }
