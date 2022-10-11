7c1754e2b6447afdce31dc05882bb36026ac470b
kimnamhyeon <knh306@naver.com>
2022-10-12 06:49:35 +0900

    Feature_Week07_Tries:insert_&_contains

--- Main.kt
+++ Main.kt
@@ -1,8 +1,7 @@
 fun main() {
-    var a = 1..10 step 2
-    var b = 10 downTo 1 step 3
-    var c = (1..5).reversed() step 2
-    println(a.toList())
-    println(b.toList())
-    println(c.toList())
+    val trie = Trie<Char>()
+    trie.insert("cute".toList())
+    if (trie.contains("cute".toList())) {
+        println("cute is in the trie")
+    }
 }
