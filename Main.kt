7c3f60f1e2999975e1934b494777235afc650c6d
kimnamhyeon <knh306@naver.com>
2022-10-12 06:58:50 +0900

    Feature_Week07_Tries:remove

--- Main.kt
+++ Main.kt
@@ -1,7 +1,15 @@
 fun main() {
     val trie = Trie<Char>()
+    trie.insert("cut")
     trie.insert("cute")
-    if (trie.contains("cute")) {
-        println("cute is in the trie")
-    }
+    println("\n*** Berfore removing ***")
+    assert(trie.contains("cut"))
+    println("\"cut\" is in the trie")
+    assert(trie.contains("cute"))
+    println("\"cute\" is in the trie")
+    println("\n*** After removing cut ***")
+    trie.remove("cut")
+    assert(!trie.contains("cut"))
+    assert(trie.contains("cute"))
+    println("\"cute\" is still in the trie")
 }
