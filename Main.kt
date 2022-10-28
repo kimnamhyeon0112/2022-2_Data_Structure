cb1941ecc5ed87ef33e63715a2f911b12d5758bf
kimnamhyeon <knh306@naver.com>
2022-10-28 21:24:22 +0900

    Feature_Week09_Priority_Queues:Comparator_Main.kt

--- Main.kt
+++ Main.kt
@@ -1,7 +1,15 @@
 fun main() {
-    val priorityQueue = MaxPriorityQueue<String>()
+    val stringLengthComparator = object : Comparator<String> {
+        override fun compare(o1: String?, o2: String?): Int {
+            val length1 = o1?.length ?: -1
+            val length2 = o2?.length ?: -1
+            return length1 - length2
+        }
+    }
 
-    arrayListOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine").forEach {
+    val priorityQueue = PriorityQueue<String>(stringLengthComparator)
+
+    arrayListOf("one", "two", "three", "forty", "five", "six", "seven", "eight", "nine").forEach {
         priorityQueue.enqueue(it)
     }
 
