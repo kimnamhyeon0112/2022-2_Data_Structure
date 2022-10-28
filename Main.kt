3f71c5ac3dbfb19f51ede097ab01e1074c95c49a
kimnamhyeon <knh306@naver.com>
2022-10-28 14:02:02 +0900

    Feature_Week09_Comparator_GenericType_Comparator

--- Main.kt
+++ Main.kt
@@ -1,3 +1,12 @@
+object MyStringComparator : Comparator<String> {
+    override fun compare(o1: String, o2: String): Int {
+        if (o1.isEmpty() || o2.isEmpty())
+            return o1.length - o2.length
+        return o1[0] - o2[0]
+    }
+}
+
 fun main() {
-    println(listOf("aaa", "bb", "c").sortedWith(compareBy { it.length }))
+    println("a".compareTo("bbb"))
+    println(listOf("cccccccc", "azz", "z", "abb").sortedWith(MyStringComparator))
 }
