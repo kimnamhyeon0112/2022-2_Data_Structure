767ae966a8ef4109e0840b23883a1488e300d4b1
kimnamhyeon <knh306@naver.com>
2022-10-28 14:10:33 +0900

    Feature_Week09_MinHeap_Main.kt

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     val array = arrayListOf(1, 12, 3, 4, 1, 6, 8, 7)
-    val h = MaxHeap<Int>()
+    val h = MinHeap<Int>()
     h.heapify(array)
     while (!h.isEmpty) {
         println(h.remove())
