2fcf98343ff197c39bcdbd9832e5768c4d768471
kimnamhyeon <knh306@naver.com>
2022-10-28 14:12:54 +0900

    Feature_Week09_Heap:merge_Main.kt

--- Main.kt
+++ Main.kt
@@ -1,8 +1,11 @@
 fun main() {
-    val array = arrayListOf(1, 12, 3, 4, 1, 6, 8, 7)
-    val h = MinHeap<Int>()
-    h.heapify(array)
-    while (!h.isEmpty) {
-        println(h.remove())
-    }
+    val arr1 = arrayListOf(1, 12, 3, 4)
+    val arr2 = arrayListOf(1, 6, 8, 7)
+    val h1 = MinHeap<Int>()
+    val h2 = MaxHeap<Int>()
+    h1.heapify(arr1)
+    h2.heapify(arr2)
+    h1.merge(h2)
+    while (!h1.isEmpty)
+        println(h1.remove())
 }
