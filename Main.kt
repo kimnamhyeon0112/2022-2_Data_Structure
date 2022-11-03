e0d83eeb3afb7c46dd28d3f7b83cd2c6d17c1b85
kimnamhyeon <knh306@naver.com>
2022-11-03 23:13:15 +0900

    Feature_Week10_Bubble_sort

--- Main.kt
+++ Main.kt
@@ -1,16 +1,6 @@
-fun f2(): ArrayList<Int> {
-    return arrayListOf(10, 20)
-}
-
-fun f1(a: ArrayList<Int>?): Int {
-    a!!.add(a.size)
-    a.addAll(f2())
-    return a.size
-}
-
-fun main(){
-    val b = arrayListOf(5)
-    println(f1(b))
-    println(b)
-    println(f1(null))
+fun main() {
+    val list = arrayListOf(9, 4, 10, 3)
+    println("Original: $list")
+    list.bubbleSort(true)
+    println("Bubble sorted: $list")
 }
