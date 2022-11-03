1750fcc83880cc03dd6c4a25a8098687c67f5e32
kimnamhyeon <knh306@naver.com>
2022-11-03 22:09:04 +0900

    Duplicate_Naming_change

--- Week02/test19.kt
+++ Week02/test19.kt
@@ -1,4 +1,4 @@
-class Person(
+class Person1(
     private var age: Int,
     public var name: String="Noname",
 )
@@ -7,6 +7,6 @@ class Person(
 
 fun main()
 {
-    var p1: Person = Person(10)
+    var p1: Person1 = Person1(10)
     println(p1.name)
 }
