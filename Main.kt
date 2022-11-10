6dedb88067ac5670eca1b17374b3d0eafb2dc661
kimnamhyeon <knh306@naver.com>
2022-11-10 15:12:27 +0900

    Feature_Week11_Merge_sort:Iterable_merge

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val list = listOf(7, 2, 6, 3, 9)
-    println("Original: $list")
-    val result = list.mergeSort()
+    val list1 = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    val list2 = listOf(1, 3, 4, 5, 5, 6, 7, 7)
+    val result = merge(list1, list2)
     println("Merge sorted: $result")
 }
--- MergeSort.kt
+++ MergeSort.kt
@@ -37,3 +37,53 @@ private fun<T: Comparable<T>> merge(left: List<T>, right: List<T>): List<T> {
         result.addAll((right.subList(rightIndex, right.size)))
     return result
 }
+
+private fun <T> Iterator<T>.nextOrNull(): T? {
+    return if (this.hasNext()) this.next() else null
+}
+
+fun <T: Comparable<T>> merge(
+    first: Iterable<T>,
+    second: Iterable<T>
+): Iterable<T> {
+    val result = mutableListOf<T>()
+    val firstIterator = first.iterator()
+    val secondIterator = second.iterator()
+
+    if (!firstIterator.hasNext()) return second
+    if (!secondIterator.hasNext()) return first
+
+    var firstEl = firstIterator.nextOrNull()
+    var secondEl = secondIterator.nextOrNull()
+
+    while (firstEl != null && secondEl != null) {
+        when {
+            firstEl < secondEl -> {
+                result.add(firstEl)
+                firstEl = firstIterator.nextOrNull()
+            }
+
+            secondEl < firstEl -> {
+                result.add(secondEl)
+                secondEl = secondIterator.nextOrNull()
+            }
+
+            else -> {
+                result.add(firstEl)
+                result.add(secondEl)
+                firstEl = firstIterator.nextOrNull()
+                secondEl = secondIterator.nextOrNull()
+            }
+        }
+    }
+
+    while (firstEl != null) {
+        result.add(firstEl)
+        firstEl = firstIterator.nextOrNull()
+    }
+    while (secondEl != null) {
+        result.add(secondEl)
+        secondEl = secondIterator.nextOrNull()
+    }
+    return result
+}
