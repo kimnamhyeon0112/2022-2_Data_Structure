0d7ea67340c0e6741d9af5119779feacc5082e67
kimnamhyeon <knh306@naver.com>
2022-11-10 15:28:33 +0900

    Feature_Week11_Radix_sort:LSD

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val list1 = listOf(1, 2, 3, 4, 5, 6, 7, 8)
-    val list2 = listOf(1, 3, 4, 5, 5, 6, 7, 7)
-    val result = merge(list1, list2)
-    println("Merge sorted: $result")
+    val list = arrayListOf(88, 410, 1772, 20)
+    println("Original: $list")
+    list.radixSort()
+    println("Merge sorted: $list")
 }
