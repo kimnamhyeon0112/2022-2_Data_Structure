823705f129655370571953217aa78ca0d02d5f5c
kimnamhyeon <knh306@naver.com>
2022-11-10 15:01:44 +0900

    Feature_Week11_Merge_sort

--- Main.kt
+++ Main.kt
@@ -1,23 +1,6 @@
-import java.util.*
-
-class CollectionItem(
-    val value: Int = Random().nextInt(1000),
-    val name: String = UUID.randomUUID().toString()
-) {
-    override fun toString(): String {
-        return "value = $value, name = $name"
-    }
-}
-
 fun main() {
-    val list = mutableListOf<CollectionItem>().apply {
-        for (i in 0 until 1000) {
-            add(CollectionItem())
-        }
-    }
-
-    val reduced: Int = list
-        .map { it.value }
-        .reduce { acc, value -> acc + value }
-    println(reduced)
+    val list = listOf(7, 2, 6, 3, 9)
+    println("Original: $list")
+    val result = list.mergeSort()
+    println("Merge sorted: $result")
 }
