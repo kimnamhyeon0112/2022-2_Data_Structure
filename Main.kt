d3569bf992d883896d289e142e7297059db8c8cb
kimnamhyeon <knh306@naver.com>
2022-11-10 14:26:35 +0900

    Feature_Week11_Filter

--- Main.kt
+++ Main.kt
@@ -1,6 +1,21 @@
+import java.util.*
+
+class CollectionItem(
+    val value: Int = Random().nextInt(1000),
+    val name: String = UUID.randomUUID().toString()
+) {
+    override fun toString(): String {
+        return "value = $value, name = $name"
+    }
+}
+
 fun main() {
-    val list = arrayListOf(9, 4, 10, 3)
-    println("Original: $list")
-    list.insertionSort(true)
-    println("Insertion sorted: $list")
+    val list = mutableListOf<CollectionItem>().apply {
+        for (i in 0 until 1000) {
+            add(CollectionItem())
+        }
+    }
+
+    val filtered: List<CollectionItem> = list.filter { it.value <= 500 }
+    println(filtered.size)
 }
