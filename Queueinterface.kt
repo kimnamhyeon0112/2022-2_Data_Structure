621ed2b3c8ceb9209a51441e83e92e160f5ca356
kimnamhyeon <knh306@naver.com>
2022-11-15 03:03:06 +0900

    Queueinterface.kt modified

---
+++ Queueinterface.kt
@@ -0,0 +1,9 @@
+/*fun QueueInterface<T>.reverse() {
+    val aux = Stack<T>()
+    var next = this.dequeue()
+    while (next != null) {
+        aux.push(next)
+        next = this.dequeue()
+    }
+    var next2 = aux.pop()
+}*/
