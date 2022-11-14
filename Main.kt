6b9e143aa38a46144ccdec0d2a913b481397d333
kimnamhyeon <knh306@naver.com>
2022-11-15 03:21:48 +0900

    Feature_Week12_Label

--- Main.kt
+++ Main.kt
@@ -1,8 +1,35 @@
 fun main() {
-    val text = StringBuilder().apply {
-        for (letter in 'A'..'Z') {
-            append(letter)
+    fun foo() {
+        listOf(1, 2, 3, 4, 5).forEach() {
+            if (it == 3) return
+            print(it)
         }
-    }.toString()
-    println(text)
+        println("this point is unreachable")
+    }
+
+    fun poo() {
+        listOf(1, 2, 3, 4, 5).forEach lit@{
+            if (it == 3) return@lit
+            print(it)
+        }
+        print(" done with explicit label")
+    }
+
+    fun goo() {
+        listOf(1, 2, 3, 4, 5).forEach {
+            if (it == 3) return@forEach
+            print(it)
+        }
+        print(" done with implicit label")
+    }
+
+    fun roo() {
+        run loop@{
+            listOf(1, 2, 3, 4, 5).forEach {
+                if (it == 3) return@loop
+                print(it)
+            }
+        }
+        print(" done with nested loop")
+    }
 }
