30a4f3a350720891e4ec739a5721bc6f4dcac0b3
kimnamhyeon <knh306@naver.com>
2022-11-15 03:10:14 +0900

    Feature_Week12_StringBuilder:with

--- Main.kt
+++ Main.kt
@@ -1,13 +1,10 @@
-fun main(){
+fun main() {
     val stringBuilder = StringBuilder()
-
-    stringBuilder.append("kotlin")
-    stringBuilder.append("program")
-    println("append 확인: " + stringBuilder.toString())
-
-    stringBuilder.insert(0, "hello")
-    println("insert 확인: " + stringBuilder.toString())
-
-    stringBuilder.delete(0, 5)
-    println("delete 확인: " + stringBuilder.toString())
+    val text = with(stringBuilder) {
+        for (letter in 'A'..'Z') {
+            append(letter)
+        }
+        toString()
+    }
+    println(text)
 }
