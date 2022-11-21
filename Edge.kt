71b362897f00e902b8ed573002eacab5dffaa295
kimnamhyeon <knh306@naver.com>
2022-11-22 01:25:44 +0900

    Feature_Week13_Graph:Adjacency_List&Matrix

---
+++ Edge.kt
@@ -0,0 +1,5 @@
+data class Edge<T>(
+    val source: Vertex<T>,
+    val destination: Vertex<T>,
+    val weight: Double? = null
+)
--- Main.kt
+++ Main.kt
@@ -1,25 +1,29 @@
-import java.util.Stack
+fun getGraph(): AdjacencyList<String> {
+    val graph = AdjacencyList<String>()
+    val singapore = graph.createVertex("Singapore")
+    val tokyo = graph.createVertex("tokyo")
+    val hongKong = graph.createVertex("hongKong")
+    val detroit = graph.createVertex("detroit")
+    val sanFrancisco = graph.createVertex("sanFrancisco")
+    val washingtonDC = graph.createVertex("washingtonDC")
+    val austinTexas = graph.createVertex("austinTexas")
+    val seattle = graph.createVertex("seattle")
+    graph.add(EdgeType.UNDIRECTED, singapore, hongKong, 300.0)
+    graph.add(EdgeType.UNDIRECTED, singapore, tokyo, 500.0)
+    graph.add(EdgeType.UNDIRECTED, hongKong, tokyo, 250.0)
+    graph.add(EdgeType.UNDIRECTED, tokyo, detroit, 450.0)
+    graph.add(EdgeType.UNDIRECTED, tokyo, washingtonDC, 300.0)
+    graph.add(EdgeType.UNDIRECTED, hongKong, sanFrancisco, 600.0)
+    graph.add(EdgeType.UNDIRECTED, detroit, austinTexas, 50.0)
+    graph.add(EdgeType.UNDIRECTED, austinTexas, washingtonDC, 292.0)
+    graph.add(EdgeType.UNDIRECTED, sanFrancisco, washingtonDC, 337.0)
+    graph.add(EdgeType.UNDIRECTED, washingtonDC, seattle, 277.0)
+    graph.add(EdgeType.UNDIRECTED, sanFrancisco, seattle, 218.0)
+    graph.add(EdgeType.UNDIRECTED, austinTexas, sanFrancisco, 297.0)
+    return graph
+}
 
-fun main() {
-    while (true) {
-        val line: String = readLine() ?: break
-        if (line.isEmpty()) break
-        val st = Stack<Char>()
-        var previousChar: Char? = null
-        var totalSlice = 0
-        line.forEach {
-            if (it == ')') {
-                totalSlice += if (previousChar == '(') {
-                    st.pop()
-                    st.size
-                } else {
-                    st.pop()
-                    1
-                }
-            }
-            else st.push(it)
-            previousChar = it
-        }
-        println(totalSlice)
-    }
+fun main(){
+    val graph = getGraph()
+    println(graph)
 }
