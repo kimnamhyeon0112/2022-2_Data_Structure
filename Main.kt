abdd27370613928315808f9e26a229a6c300d59a
kimnamhyeon <knh306@naver.com>
2022-11-29 16:51:11 +0900

    Feature_Week14_Baekjoon_no.2606

--- Main.kt
+++ Main.kt
@@ -1,14 +1,30 @@
-fun main() {
-    val graph = AdjacencyMatrix<String>()
-    val A = graph.createVertex("A")
-    val B = graph.createVertex("B")
-    val C = graph.createVertex("C")
-    val D = graph.createVertex("D")
-    graph.add(EdgeType.UNDIRECTED, A, B, 1.0);graph.add(EdgeType.UNDIRECTED, B, C, 1.0)
-    graph.add(EdgeType.UNDIRECTED, C, A, 1.0);graph.add(EdgeType.UNDIRECTED, C, D, 1.0)
+import java.util.*
 
-    println(graph.hasCycle(A))
-    println(graph.hasCycle(B))
-    println(graph.hasCycle(C))
-    println(graph.hasCycle(D))
+fun main() {
+    val numVertex = readLine()!!.toInt()
+    val numEdge = readLine()!!.toInt()
+    val graph = Array(numVertex) { ArrayList<Int>() }
+
+    fun addEdge(s: Int, t: Int) {
+        graph[s].add(t)
+        graph[t].add(s)
+    }
+
+    fun getConnected(s: Int, visited: ArrayList<Int>) {
+        if (visited.contains(s))
+            return
+        visited.add(s)
+        graph[s].forEach {
+            getConnected(it, visited)
+        }
+    }
+
+    repeat(numEdge) {
+        val (src, tgt) = readLine()!!.split(" ").map { it.toInt() }
+        addEdge(src - 1, tgt - 1)
+    }
+
+    val visited = ArrayList<Int>()
+    getConnected(0, visited)
+    println(visited.size - 1)
 }
