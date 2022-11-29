8190a21da93ff689c9bb662020d093560a13f3ec
kimnamhyeon <knh306@naver.com>
2022-11-29 17:08:18 +0900

    Feature_Week14_Dijkstra

--- Main.kt
+++ Main.kt
@@ -1,30 +1,24 @@
-import java.util.*
-
 fun main() {
-    val numVertex = readLine()!!.toInt()
-    val numEdge = readLine()!!.toInt()
-    val graph = Array(numVertex) { ArrayList<Int>() }
-
-    fun addEdge(s: Int, t: Int) {
-        graph[s].add(t)
-        graph[t].add(s)
+    val graph = AdjacencyList<String>()
+    val A = graph.createVertex("A")
+    val B = graph.createVertex("B")
+    val C = graph.createVertex("C")
+    val D = graph.createVertex("D")
+    val E = graph.createVertex("E")
+    val F = graph.createVertex("F")
+    val G = graph.createVertex("G")
+    val H = graph.createVertex("H")
+    graph.add(EdgeType.DIRECTED, A, B, 8.0); graph.add(EdgeType.DIRECTED, A, G, 1.0)
+    graph.add(EdgeType.DIRECTED, A, F, 9.0); graph.add(EdgeType.DIRECTED, B, E, 1.0)
+    graph.add(EdgeType.DIRECTED, B, F, 3.0); graph.add(EdgeType.DIRECTED, C, B, 3.0)
+    graph.add(EdgeType.DIRECTED, C, E, 1.0); graph.add(EdgeType.DIRECTED, E, B, 1.0)
+    graph.add(EdgeType.DIRECTED, E, D, 2.0); graph.add(EdgeType.DIRECTED, F, A, 2.0)
+    graph.add(EdgeType.DIRECTED, G, C, 3.0); graph.add(EdgeType.DIRECTED, H, G, 5.0)
+    graph.add(EdgeType.DIRECTED, H, F, 2.0)
+    val dijkstra = Dijkstra(graph)
+    val pathsFromA = dijkstra.shortestPath(A)
+    val path = dijkstra.shortestPath(D, pathsFromA)
+    path.forEach {
+        println("${it.source.data} --|${it.weight ?: 0.0}|--> ${it.destination.data}")
     }
-
-    fun getConnected(s: Int, visited: ArrayList<Int>) {
-        if (visited.contains(s))
-            return
-        visited.add(s)
-        graph[s].forEach {
-            getConnected(it, visited)
-        }
-    }
-
-    repeat(numEdge) {
-        val (src, tgt) = readLine()!!.split(" ").map { it.toInt() }
-        addEdge(src - 1, tgt - 1)
-    }
-
-    val visited = ArrayList<Int>()
-    getConnected(0, visited)
-    println(visited.size - 1)
 }
